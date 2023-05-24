5b1faece7c2587a4873d191bc798e2842d890b1d
nebulemz <lemueljohnxd3@gmail.com>
2023-05-24 11:40:19 +0800

    Revert "awit"
    This reverts commit fd959d037887a4dfea85b15b43e831fd69188fb1.

--- costpredict.php
+++ costpredict.php
@@ -43,10 +43,7 @@ require_once('partials/_head.php');
           </div>
         </div>
       </div>
-      <!-- Footer -->
-      <?php
-      require_once('partials/_footer.php');
-      ?>
+      
     </div>
   </div>
   </body>
@@ -94,4 +91,8 @@ require_once('partials/_head.php');
     return true;
   }
 </script>
+<!-- Footer -->
+<?php
+require_once('partials/_footer.php');
+?>
 </html>
